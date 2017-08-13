8021c71f8d5b91457f61c22d8bcc2695c482514b
riojack <ayjay122@gmail.com>
2017-08-13 17:14:38 -0500

    Move magic 3 into a constant.

--- src/main/java/com/pillartechnology/tdd/Fizz.java
+++ src/main/java/com/pillartechnology/tdd/Fizz.java
@@ -2,11 +2,12 @@ package com.pillartechnology.tdd;
 
 public class Fizz {
     public static final String FIZZ = "fizz";
+    public static final int FIZZ_DIVIDER = 3;
 
     public String fizz(int fizzableNumber) {
         String answer = Integer.toString(fizzableNumber);
 
-        if (fizzableNumber % 3 == 0) {
+        if (fizzableNumber % FIZZ_DIVIDER == 0) {
             answer = FIZZ;
         }
 
