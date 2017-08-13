fb1ec39714710e266e564c7490feb8711eea2e37
riojack <ayjay122@gmail.com>
2017-08-13 17:02:59 -0500

    Refactor test repetition into setUp method

--- src/test/java/com/pillartechnology/tdd/FizzTest.java
+++ src/test/java/com/pillartechnology/tdd/FizzTest.java
@@ -1,21 +1,25 @@
 package com.pillartechnology.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzTest {
+    private Fizz fizz;
+
+    @Before
+    public void setUp() {
+        fizz = new Fizz();
+    }
+
     @Test
     public void shouldReturnOneWhenOneIsGivenToFizzMethod() {
-        Fizz fizz = new Fizz();
-
         assertEquals("1", fizz.fizz(1));
     }
 
     @Test
     public void shouldReturnTwoWhenTwoIsGivenToFizzMethod() {
-        Fizz fizz = new Fizz();
-
         assertEquals("2", fizz.fizz(2));
     }
 }
